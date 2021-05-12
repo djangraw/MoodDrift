849f2191a4bac52650276d8230647586547790c4
shotgunosine <adenosine@gmail.com>
2021-05-12 14:26:03 -0400

    fix paths for cohen's d

--- scripts/ProduceAllResults.py
+++ scripts/ProduceAllResults.py
@@ -1399,8 +1399,8 @@ print(f'Batch {batchName}: Mood ratings were locked in or moved on {pctLockedOrM
 
 # get cohen's D based on https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4324017/
 
-dat = pd.read_csv('PassageOfTimeDysphoria/Data/OutFiles/Mmi-AllOpeningRestAndRandom_Ratings.csv', index_col=0)
-fits = pd.read_csv('PassageOfTimeDysphoria/Data/OutFiles/Mmi-AllOpeningRestAndRandom_pymerFit-full.csv', index_col=0)
+dat = pd.read_csv(f'{dataDir}/Mmi-AllOpeningRestAndRandom_Ratings.csv', index_col=0)
+fits = pd.read_csv(f'{dataDir}/Mmi-AllOpeningRestAndRandom_pymerFit-full.csv', index_col=0)
 mean_duration = (dat.loc[dat.iBlock == 0].groupby('participant')['time'].max().mean()/60)
 std_final_rating = dat.loc[(dat.iBlock == 0)].groupby('participant').rating.last().std()
 print(f"Estimates for the effect size of the full LME for all online participants (AllOpeningRestAndRandom_Ratings) at the mean duration ({mean_duration:0.3g} minutes).")
